1577a4a1decde55502616116e5e17c2d1112b6a5
julianWww <julian.wandhoven@gmail.com>
2021-11-23 00:49:57 +0100

    moved everything to pytorch

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-import numpy as np
+import torch
+import time, warnings
+
+warnings.filterwarnings('ignore')
 
 def createBaseComplexImageMatrix(start_real, start_imag, end_real, end_imag, step):
-    "creates a 2d array of comples numbers using the start, end and step like np.arange"
-    real, imag = np.meshgrid(
-        np.arange(start_real, end_real+step, step),
-        np.arange(start_imag, end_imag+step, step))
+    real, imag = torch.meshgrid(
+        torch.arange(start_real, end_real+step, step),
+        torch.arange(start_imag, end_imag+step, step)
+        )
     return real + imag * complex(0,1)
 
+def generatorStep(z, c):
+    "return z^2+c"
+    mask = torch.where(torch.abs(z) <= 2)
+    z_masked = z[mask]
+    c_masked = c[mask]
+    z[mask] =  z_masked**2+c_masked
+    return z
 
-print(createBaseComplexImageMatrix(-1,-1,1,1,1))
+def generateSet(c, interations):
+    "generate the set for values c for interations interations"
+    z = torch.clone(c)
+    for iteration in range(interations):
+        z = generatorStep(z,c)
+    return torch.abs(z) < 2
+    
+
+
+t0 = time.time()
+arr = createBaseComplexImageMatrix(-2,-2,2,2,0.001)
+out = generateSet(arr, 100)
+t1 = time.time()
+print(t1-t0)
+
+
+
+# 21.363631010055542
